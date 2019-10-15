39694a9c03ad43fd47bb71f57486713dd5dffa85
Njörd <davidbillamboz@gmail.com>
2019-10-15 23:30:13 +0200

    Remove broken stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -94,7 +94,6 @@ module.exports = {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
         enableIdentityWidget: true,
-        // modulePath: `${__dirname}/src/cms/cms.js`,
       },
     },
   ],
--- src/cms/cms.js
+++
@@ -1,7 +0,0 @@
-import CMS from 'netlify-cms-app';
-
-import HeaderPreview from './preview-templates/HeaderPreview';
-// import FooterPreview from './preview-templates/FooterPreview';
-
-CMS.registerPreviewTemplate('header', HeaderPreview);
-// CMS.registerPreviewTemplate('footer', FooterPreview);

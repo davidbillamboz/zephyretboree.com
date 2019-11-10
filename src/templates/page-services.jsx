223c604f02b64874cf4a22fd99bc797578eee003
Njörd <davidbillamboz@gmail.com>
2019-11-10 23:57:56 +0100

    Implement simulator

--- src/templates/page-services.jsx
+++ src/templates/page-services.jsx
@@ -5,12 +5,14 @@ import Title from '../components/Title';
 import SubTitle from '../components/SubTitle';
 import Markdown from '../components/Markdown';
 import ServicesServices from '../components/services/Services';
+import ServicesSimulator from '../components/services/Simulator';
 
 const PageServices = ({ data }) => {
   const {
     intro: { title, subTitle, text },
     contactButton,
     services,
+    simulator,
   } = data.page.frontmatter;
 
   return (
@@ -22,6 +24,9 @@ const PageServices = ({ data }) => {
       </section>
       <section className="section">
         <ServicesServices services={services} contactButton={contactButton} />
+      </section>
+      <section className="section">
+        <ServicesSimulator {...simulator} />
       </section>
     </div>
   );
@@ -38,6 +43,7 @@ PageServices.propTypes = {
         }).isRequired,
         contactButton: PropTypes.shape({}).isRequired,
         services: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+        simulator: PropTypes.shape({}).isRequired,
       }).isRequired,
     }).isRequired,
   }).isRequired,
@@ -71,6 +77,37 @@ export const pageQuery = graphql`
           title
           text
           tags
+        }
+        simulator {
+          title
+          subTitle
+          text
+          sliderConfig {
+            min {
+              value
+              title
+            }
+            max {
+              value
+              title
+            }
+          }
+          valuesConfig {
+            name
+            title
+            min
+            max
+          }
+          propulsions {
+            title
+            text
+            valuesText {
+              delay
+              speed
+              emissions
+              price
+            }
+          }
         }
       }
     }

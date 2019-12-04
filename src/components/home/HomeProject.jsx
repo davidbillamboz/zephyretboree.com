bdb2804598bbd36345e433dc01848f2a51b54ff4
Njörd <davidbillamboz@gmail.com>
2019-12-04 01:26:34 +0100

    Optimize images

--- src/components/home/HomeProject.jsx
+++ src/components/home/HomeProject.jsx
@@ -3,66 +3,61 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import GatsbyImage from 'gatsby-image/withIEPolyfill';
 
-const ProjectStyled = styled.div`
+const ImageMobileStyled = styled(GatsbyImage)`
+  width: 100%;
+  height: auto;
+  max-width: 250px;
+  margin: auto;
+`;
+
+const ImageTabletStyled = styled(GatsbyImage)`
+  width: 100%;
+  height: auto;
+`;
+
+const TitleStyled = styled.h4`
+  font-family: 'ZephyrEtBoree', 'Helvetica', 'Arial', sans-serif;
+  font-weight: bold;
+  font-size: 20px;
+  line-height: 20px;
+  margin: 0.75rem 0;
+
   @media (min-width: ${props => props.theme.breakpointTablet}) {
-    div {
-      width: 100%;
-    }
+    margin-top: 30px;
+    margin-bottom: 30px;
+    font-size: 25px;
+    line-height: 28px;
   }
 
-  img {
-    width: 100%;
-    height: auto;
-
-    @media (min-width: ${props => props.theme.breakpointTablet}) {
-      max-width: auto;
-    }
+  @media (min-width: 1024px) {
+    font-size: 27px;
   }
+`;
 
-  h4 {
-    font-family: 'ZephyrEtBoree', 'Helvetica', 'Arial', sans-serif;
-    font-weight: bold;
-    font-size: 20px;
-    line-height: 20px;
-    margin: 0.75rem 0;
-
-    @media (min-width: ${props => props.theme.breakpointTablet}) {
-      margin-top: 30px;
-      margin-bottom: 30px;
-      font-size: 25px;
-      line-height: 28px;
-    }
-
-    @media (min-width: 1024px) {
-      font-size: 27px;
-    }
-  }
-
-  p {
-    font-size: 16px;
+const TextStyled = styled.p`
+  font-size: 16px;
+  line-height: 25px;
+  @media (min-width: ${props => props.theme.breakpointTablet}) {
+    font-size: 18px;
     line-height: 25px;
-    @media (min-width: ${props => props.theme.breakpointTablet}) {
-      font-size: 18px;
-      line-height: 25px;
-    }
   }
 `;
 
 const Project = ({ imageHorizontal, imageVertical, title, text }) => (
-  <ProjectStyled className="column">
-    <div>
-      <GatsbyImage
+  <div className="column">
+    <div className="has-text-centered-mobile">
+      <ImageMobileStyled
         fluid={imageHorizontal.childImageSharp.fluid}
         className="is-hidden-tablet"
       />
-      <GatsbyImage
+      <ImageTabletStyled
         fluid={imageVertical.childImageSharp.fluid}
         className="is-hidden-mobile"
       />
-      <h4>{title}</h4>
-      <p>{text}</p>
+      <TitleStyled>{title}</TitleStyled>
+      <TextStyled>{text}</TextStyled>
     </div>
-  </ProjectStyled>
+  </div>
 );
 
 Project.propTypes = {
